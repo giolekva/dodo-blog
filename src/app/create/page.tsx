d9d3f7cdc4728ed1b0bece4c55ba8a80dc3d5c38
gio <gio@v1.dodo.cloud>
2024-10-18 14:15:15 +0000

    author + auth pr

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 async function createEntry(data: FormData) {
     'use server'
+    const user = headers().get("x-forwarded-user")!;
     const prisma = new PrismaClient();
     await prisma.entry.create({
         data: {
             title: data.get("title")!.toString(),
             contents: data.get("contents")!.toString(),
+            author: user,
         }
     });
     redirect("/");
--- src/app/entry/[id]/page.tsx
+++ src/app/entry/[id]/page.tsx
@@ -1,11 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { Entry, PrismaClient } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { ReactElement } from "react";
 
 type Params = {
     params: {
         id: string;
     };
 };
+
+function author(entry: Entry): ReactElement | null {
+    if (entry.author === null) {
+        return null;
+    }
+    return (<h6>Author: {entry.author}</h6>);
+}
 
 export default async function Page({ params } : Params) {
     'use server'
@@ -21,6 +29,7 @@ export default async function Page({ params } : Params) {
     return (
         <div>
             <h1>{entry.title}</h1>
+            {author(entry)}
             <div>
                 {entry.contents}
             </div>
