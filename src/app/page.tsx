b180687115285cfc22e6b89a97dc0e25740cc778
Giorgi Lekveishvili <lekva@gl-mbp-m1-max-2.local>
2024-10-18 17:17:08 +0400

    hello blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        Hello from dodo: App
-        <ol>
-          <li>
-            Get started by editing code directly in the browser: <a href="TODO">TODO</a>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        Hello dodo Blog!
       </main>
     </div>
   );
